01a258b462f883a31f10481083460ce33254025d
SakarRai <43678032+SakarRai@users.noreply.github.com>
2025-02-09 20:43:09 +0000

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    const yesButton = document.querySelector('#yesButton');
+    const noButton = document.querySelector('#noButton');
+    let hoverCount = 0;
+    let messageElement = null;
+
+    const messages = [
+        "Are you sure?",
+        "Really sure?",
+        "Think about it",
+        "I will be really sad..",
+        "I will be really really sad..",
+        "Wow...",
+        "You're hurting my feelings",
+        "I can't believe this",
+        "I'll give you the silent treatment",
+        "i really mean it",
+        "..hmph",
+        "",
+        "",
+        ". .",
+        ". . .",
+        ". . . .",
+        "..You're still trying",
+        "I can do this all day - Captian America",
+        "",
+
+
+    ];
+
+    const moveNoButton = () => {
+        var x = Math.random() * (window.innerWidth - noButton.offsetWidth);
+        var y = Math.random() * (window.innerHeight - noButton.offsetHeight);
+        
+        noButton.style.position = 'absolute';
+        noButton.style.left = `${x}px`;
+        noButton.style.top = `${y}px`;
+
+        hoverCount++;
+        if (hoverCount >= 3 && hoverCount <= 21) {
+            if (messageElement) {
+                document.body.removeChild(messageElement);
+            }
+            messageElement = document.createElement('h1');
+            messageElement.innerText = messages[hoverCount - 3]; // Adjust array index
+            messageElement.style.color = 'rgb(253, 77, 77)';
+            messageElement.style.textShadow = '2px 2px 2px rgb(128, 240, 255, 1)';
+            messageElement.style.boxShadow = '100';
+            messageElement.style.letterSpacing = '3px';
+            messageElement.sbackgroundColorletterSpacing = '3px';
+            messageElement.style.textAlign = 'center';
+            messageElement.style.position = 'fixed';
+            messageElement.style.top = '50%';
+            messageElement.style.left = '50%';
+            messageElement.style.transform = 'translate(-50%, -50%)';
+            document.body.appendChild(messageElement);
+
+            setTimeout(() => {
+                if (messageElement) {
+                    document.body.removeChild(messageElement = 21);
+                    messageElement = null;
+                }
+            }, 100000);
+        }
+    };
+
+    yesButton.addEventListener('click', () => {
+        window.location.assign('yes.html');
+    });
+
+    noButton.addEventListener('mouseenter', moveNoButton);
+});
